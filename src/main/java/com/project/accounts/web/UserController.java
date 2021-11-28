dbc01dbbf85811c0a2ac9d63111bbb84204192c9
sebastianpalarczyk <s.palarczyk12@gmail.com>
2021-11-28 16:01:30 +0100

    make it better

--- src/main/java/com/project/accounts/web/UserController.java
+++ src/main/java/com/project/accounts/web/UserController.java
@@ -13,7 +13,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Controller
-@RequestMapping(value = "/user")
+@RequestMapping(value = "https://app-palarczyk.herokuapp.com/user")
 public class UserController {
 
     private final UserService userService;
